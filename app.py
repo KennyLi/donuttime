939f4dbfe435cc0b0746b110b4aa1b95232ab47c
Kenny <lli15@stuy.edu>
2019-05-06 22:57:50 -0400

    added starter kit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os, urllib, json
+
+from flask import Flask, request, render_template, \
+     flash, session, url_for, redirect
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
+
+#root route
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
