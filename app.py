0070d2d5777081ad3b4667a5a0e1fe682530a526
Kenny <lli15@stuy.edu>
2019-05-15 22:07:56 -0400

    blob

--- app.py
+++ app.py
@@ -12,8 +12,53 @@ app.secret_key = os.urandom(32)
 #root route
 @app.route("/")
 def home():
-    return render_template("draw.html")
+    try:
+        return render_template("draw.html", user=session["logged_in"])
+    except:
+        return render_template("draw.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/auth")
+def authenticate():
+    if db.auth_user(request.args["user"], request.args["password"]):
+        session["logged_in"] = request.args["user"]
+        return redirect(url_for("home"))
+    else:
+        flash("username or password is incorrect")
+        return redirect(url_for("login"))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/adduser")
+def add_user():
+    if(not request.args["user"].strip() or not request.args["password"] or not request.args["confirm_password"]):
+        flash("Please fill in all fields")
+        return redirect(url_for("register"))
+
+    if(db.check_user(request.args["user"])):
+        flash("User already exists")
+        return redirect(url_for("register"))
+
+    if(request.args["password"] != request.args["confirm_password"]):
+        flash("Passwords don't match")
+        return redirect(url_for("register"))
+
+    db.add_user(request.args["user"], request.args["password"])
+    session["logged_in"] = request.args["user"]
+    return redirect(url_for("home"))
+
+@app.route("/logout")
+def logout():
+    try:
+        for key in session.keys():
+            session.pop(key)
+    finally:
+        return redirect(url_for("home"))
 
 @app.route("/test")
 def test():
@@ -24,6 +69,7 @@ def blob():
     db.add_drawing("Kenny", request.form["drawing_name"], request.files["file"].read())
     return "Successfully Downloaded"
 
-if __name__ == "__main__":	
-    app.debug = True	
+if __name__ == "__main__":
+    app.jinja_env.auto_reload = True
+    app.debug = True
     app.run()
