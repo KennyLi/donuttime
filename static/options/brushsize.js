c28d67d6ae4bfb7fcf59e610db27692cd0923ff2
Johnson Li <jli39@stuy.edu>
2019-05-19 00:44:55 -0400

    input for brush and opacity

--- static/options/brushsize.js
+++ static/options/brushsize.js
@@ -1,9 +1,15 @@
 // BRUSH SIZE PICKER
 var brushSlider = document.getElementById('brush-slider');
-var sizeIndicator = document.getElementById('curr-brush-size');
+var sizeSelector = document.getElementById('curr-brush-size');
 
 brushSlider.addEventListener('input', (e) => {
     val = e.target.value;
     brushSize = parseInt(val);
-    sizeIndicator.innerHTML = val;
+    sizeSelector.value = val;
 });
+
+sizeSelector.addEventListener('input', (e) => {
+    val = e.target.value;
+    brushSize = parseInt(val);
+    brushSlider.value = val;
+})
--- static/options/opacity.js
+++ static/options/opacity.js
@@ -1,10 +1,17 @@
 var opacitySlider = document.getElementById('opacity-slider');
-var opacityIndicator = document.getElementById('curr-opacity');
+var opacitySelector = document.getElementById('curr-opacity');
 
 opacitySlider.addEventListener('input', (e) => {
     val = e.target.value;
     opacity = parseInt(val) / 100;
-    opacityIndicator.innerHTML = val;
+    opacitySelector.value = val;
     color[3] = opacity; //update color opacity
 })
-// REPEATED CODE. MAKE BETTER LATER
+
+opacitySelector.addEventListener('input', (e) => {
+    val = e.target.value;
+    opacity = parseInt(val) / 100;
+    opacitySlider.value = val;
+    color[3] = opacity; //update color opacity
+})
+
