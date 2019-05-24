ff393a0f3096aa4275eb5b5dba8e9e9ec3592a75
jiayang <jchen59@stuy.edu>
2019-05-24 16:07:28 -0400

    added adding layers

--- static/layers.js
+++ static/layers.js
@@ -2,6 +2,7 @@
 var canvases = {0: canvas};
 var canvasesOrdering = [0];
 var canvasIDCounter = 0;
+var layerForm = document.forms["layer-form"];
 var newLayer = function() {
     let c = document.createElement("canvas");
     c.className += " helpercanvas";
@@ -12,20 +13,34 @@ var newLayer = function() {
     content.insertBefore(c, cursorCanvas)
     canvases[c.canvasid] = c
     canvasesOrdering.push(c.canvasid);
+    let newDiv = document.createElement("div");
+    //Add radio button div
+    var radio = document.createElement("input")
+    radio.type = "radio";
+    radio.name = "layer";
+    radio.value = (canvasIDCounter).toString();
+    newDiv.addEventListener("click", function() {
+        changeActiveLayer(this.children[0]);
+    });
+    newDiv.appendChild(radio);
+    newDiv.innerHTML +=  " " + (canvasIDCounter + 1).toString() + "<br>";
+    layerForm.insertBefore(newDiv, layerForm.firstChild);
+
+
 }
 
-newLayer()
-newLayer()
-var radios = document.forms["layer-form"].children;
+var radios = layerForm.children;
 for(var i = 0; i < radios.length; i++) {
-    radios[i].onclick = function() {
-        changeActiveLayer(this);
-    }
+    radios[i].addEventListener("click", function() {
+        changeActiveLayer(this.children[0]);
+    });
 }
 
 var changeActiveLayer = function(e) {
     canvas = canvases[parseInt(e.value)];
     ctx = canvas.getContext("2d");
-    console.log(e.value);
-    console.log(canvases)
 }
+
+//Adding Layers
+var addLayerButton = document.getElementById("addlayer");
+addLayerButton.addEventListener("click", newLayer);
