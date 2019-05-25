cc9e11b505098a5e1502a73a737e9ce898055c7a
jiayang <jchen59@stuy.edu>
2019-05-25 15:56:32 -0400

    Add deleting layers

--- static/layers.js
+++ static/layers.js
@@ -1,6 +1,4 @@
 //Layers
-var canvases = {0: canvas};
-var canvasesOrdering = [0];
 var canvasIDCounter = 0;
 var layerForm = document.forms["layer-form"];
 var newLayer = function() {
@@ -13,34 +11,49 @@ var newLayer = function() {
     content.insertBefore(c, cursorCanvas)
     canvases[c.canvasid] = c
     canvasesOrdering.push(c.canvasid);
+    let divContainer = document.createElement("div");
     let newDiv = document.createElement("div");
-    //Add radio button div
-    var radio = document.createElement("input")
-    radio.type = "radio";
-    radio.name = "layer";
-    radio.value = (canvasIDCounter).toString();
+    //Add new layer div
     newDiv.addEventListener("click", function() {
-        changeActiveLayer(this.children[0]);
+        changeActiveLayer(this);
     });
-    newDiv.appendChild(radio);
-    newDiv.innerHTML +=  " " + (canvasIDCounter + 1).toString() + "<br>";
-    layerForm.insertBefore(newDiv, layerForm.firstChild);
+    newDiv.setAttribute("value",c.canvasid.toString());
+    newDiv.innerHTML +=  (c.canvasid + 1).toString() + "<br>";
+    let deleteDiv = document.createElement("div");
+    deleteDiv.innerHTML = "X";
+    deleteDiv.addEventListener("click", function(e) {
+        deleteLayer(c.canvasid);
+    });
+    divContainer.className += " div-container";
+    divContainer.appendChild(newDiv);
+    divContainer.appendChild(deleteDiv);
+    layerForm.insertBefore(divContainer, layerForm.firstChild);
 
+    divs[c.canvasid] = divContainer;
 
 }
 
-var radios = layerForm.children;
-for(var i = 0; i < radios.length; i++) {
-    radios[i].addEventListener("click", function() {
-        changeActiveLayer(this.children[0]);
-    });
-}
+var divs = {0: layerForm.children[0]};
+divs[0].addEventListener("click", function() {
+    changeActiveLayer(this.children[0]);
+});
 
 var changeActiveLayer = function(e) {
-    canvas = canvases[parseInt(e.value)];
+    canvas = canvases[parseInt(e.attributes.value.value)];
     ctx = canvas.getContext("2d");
 }
 
 //Adding Layers
 var addLayerButton = document.getElementById("addlayer");
 addLayerButton.addEventListener("click", newLayer);
+
+
+//Deleting Layers
+var deleteLayer = function(cid) {
+    canvases[cid].remove()
+    canvases[cid] = undefined;
+    canvasesOrdering.splice(canvasesOrdering.indexOf(cid), 1);
+    divs[cid].remove();
+    divs[cid] = undefined;
+    changeActiveLayer(divs[0].children[0])
+}
