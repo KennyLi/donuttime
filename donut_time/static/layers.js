0db438c644bf026b93bb60599b222434c65b3c69
jiayang <jchen59@stuy.edu>
2019-05-30 19:31:56 -0400

    Added styling for layers

--- donut_time/static/layers.js
+++ donut_time/static/layers.js
@@ -15,19 +15,21 @@ var newLayer = function() {
     let newLi = document.createElement("li");
     let divContainer = document.createElement("div");
     newLi.appendChild(divContainer);
+    newLi.className += " list-group-item";
     let newDiv = document.createElement("div");
     //Add new layer div
-    newDiv.addEventListener("click", function() {
-        changeActiveLayer(this);
+    newLi.addEventListener("click", function() {
+        changeActiveLayer(this.firstElementChild.firstElementChild);
     });
     newDiv.setAttribute("value",c.canvasid.toString());
     newDiv.innerHTML +=  (c.canvasid + 1).toString() + "<br>";
     let deleteDiv = document.createElement("div");
     deleteDiv.innerHTML = "X";
     deleteDiv.addEventListener("click", function(e) {
+        e.stopPropagation();
         deleteLayer(c.canvasid);
-    });
-    divContainer.className += " layer-container";
+    }, true);
+    divContainer.className += " layer-container d-flex flex-row justify-content-between";
     divContainer.appendChild(newDiv);
     divContainer.appendChild(deleteDiv);
     layerForm.firstElementChild.insertBefore(newLi, layerForm.firstElementChild.firstChild);
@@ -36,14 +38,21 @@ var newLayer = function() {
     addHistory(saveStates());
 }
 
-var divs = {0: layerForm.firstElementChild.firstChild};
-divs[0].addEventListener("click", function() {
-    changeActiveLayer(this.children[0]);
+var divs = {0: layerForm.firstElementChild.firstElementChild.firstElementChild};
+console.log(divs[0])
+divs[0].parentNode.addEventListener("click", function() {
+    changeActiveLayer(this.firstElementChild.firstElementChild);
 });
+divs[0].lastChild.addEventListener("click", function(e) {
+    e.stopPropagation();
+    deleteLayer(0);
+},true);
 
 var changeActiveLayer = function(e) {
+    divs[canvas.canvasid].parentNode.className = "list-group-item";
     canvas = canvases[parseInt(e.attributes.value.value)];
     ctx = canvas.getContext("2d");
+    divs[canvas.canvasid].parentNode.className = "list-group-item active-layer";
 }
 
 //Adding Layers
@@ -53,10 +62,13 @@ addLayerButton.addEventListener("click", newLayer);
 
 //Deleting Layers
 var deleteLayer = function(cid) {
+    if (canvasesOrdering[0] == cid) {
+        return
+    }
     canvases[cid].remove()
     delete canvases[cid];
     canvasesOrdering.splice(canvasesOrdering.indexOf(cid), 1);
-    divs[cid].remove();
+    divs[cid].parentNode.remove();
     delete divs[cid];
     changeActiveLayer(divs[0].children[0]);
 }
@@ -65,9 +77,9 @@ var deleteLayer = function(cid) {
 var swapLayers = function(a,b) {
     var c = canvasesOrdering;
     [c[a],c[b]] = [c[b],c[a]];
-    swap(divs[b],divs[a]);
+    swap(divs[b].parentNode,divs[a].parentNode);
     swap(canvases[a],canvases[b]);
-    
+
 }
 
 
